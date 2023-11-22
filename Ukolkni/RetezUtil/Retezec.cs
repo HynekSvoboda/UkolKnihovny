24a6f9b198c7ef6af84c3fd3a0a66a1af8f7ad65
HynekSvoboda <97514770+HynekSvoboda@users.noreply.github.com>
2023-11-22 11:26:22 +0100

    Add files via upload

---
+++ Ukolkni/RetezUtil/Retezec.cs
@@ -0,0 +1,20 @@
+﻿namespace RetezUtil
+{
+    public class Retezec
+    {
+        static public string Smaz(string vstup)
+        {
+            foreach (char c in vstup)
+            {
+                if (c >= '0' && c <= '9') vstup=vstup.Replace(c,' ');
+            }
+
+            while (vstup.Contains("  "))
+            {
+                vstup = vstup.Replace("  ", " ");
+            }
+
+            return vstup;
+        }
+    }
+}
---
+++ Ukolkni/Ukolkni/Form1.cs
@@ -0,0 +1,17 @@
+using RetezUtil;
+
+namespace Ukolkni
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label1.Text = Retezec.Smaz(textBox1.Text);
+        }
+    }
+}
